ce47a35bc2b3497d1725562b1d37903902cf3e4f
MJ <codemike@naver.com>
2020-11-18 16:04:53 +0900

    feat - 디바이스ID 설정 기능 추가

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,7 +4,19 @@
   </div>
 </template>
 <script>
+import { T } from "./store/module/types";
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  created() {
+    this.setDeviceId();
+  },
+  methods: {
+    setDeviceId() {
+      const deviceId = String(Math.random());
+      this.$store.dispatch(T.INIT_PROGRAM, deviceId);
+      console.log("Device ID = "+  deviceId);
+    },
+  },
+};
 </script>
