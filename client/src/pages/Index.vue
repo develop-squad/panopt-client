7a606f2a9e8f69d82f1e93e4dceb73e8f52a7bdb
MJ <codemike@naver.com>
2020-11-18 16:06:06 +0900

    feat - mapbox 추가

--- client/src/pages/Index.vue
+++ client/src/pages/Index.vue
@@ -5,9 +5,29 @@
 </template>
 
 <script>
+import { T } from "../store/module/types";
+
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex",
+  mounted() {
+    this.initMapbox();
+  },
+  methods: {
+    initMapbox() {
+      let mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
+
+      mapboxgl.accessToken =
+        "pk.eyJ1IjoibW9vbmp1aGFuIiwiYSI6ImNrZXV4Y3g0dzIxeDAyeHF2NXpqZmJ0d3YifQ.mQU_y-ckbnxNzg8g9BsEKQ";
+      let map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/moonjuhan/ckeuxj63h0ooy19js1h2ezuqz",
+      });
+    },
+    drawNetwork(params){
+
+    }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
